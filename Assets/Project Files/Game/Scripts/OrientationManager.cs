7daab37d9c2361d779b7c3d56305a9a1b558aa51
dev pandiya <devpandiya@saltocorp.com>
2025-09-25 12:34:43 +0530

    new

---
+++ Assets/Project Files/Game/Scripts/OrientationManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class OrientationManager : MonoBehaviour
+{
+    void Awake()
+    {
+        SetOrientation();
+    }
+
+    void SetOrientation()
+    {
+        // Calculate physical screen size in inches
+        float dpi = Screen.dpi;
+        float widthInches = Screen.width / dpi;
+        float heightInches = Screen.height / dpi;
+        float diagonalInches = Mathf.Sqrt(widthInches * widthInches + heightInches * heightInches);
+
+        // If diagonal is greater than ~7 inches → treat as tablet
+        if (diagonalInches >= 7.0f)
+        {
+            Screen.orientation = ScreenOrientation.LandscapeLeft;
+            Debug.Log("Tablet detected → Landscape mode");
+        }
+        else
+        {
+            Screen.orientation = ScreenOrientation.Portrait;
+            Debug.Log("Phone detected → Portrait mode");
+        }
+    }
+}
